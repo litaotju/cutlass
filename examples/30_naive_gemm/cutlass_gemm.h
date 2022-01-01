707febd83dd97540dee3d5eef91e4ccaa0c59cac
Tao Li <litaotju@live.cn>
2022-01-01 16:31:13 +0800

    Add an naive gemm implementation

---
+++ examples/30_naive_gemm/cutlass_gemm.h
@@ -0,0 +1,146 @@
+#include "cutlass/gemm/device/gemm.h"
+#include <iostream>
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Define a CUTLASS GEMM template and launch a GEMM kernel.
+cudaError_t CutlassSgemmTT(
+  int M,
+  int N,
+  int K,
+  float alpha,
+  float const *A,
+  int lda,
+  float const *B,
+  int ldb,
+  float beta,
+  float *C,
+  int ldc) {
+
+  // Define type definition for single-precision CUTLASS GEMM with column-major
+  // input matrices and 128x128x8 threadblock tile size (chosen by default).
+  //
+  // To keep the interface manageable, several helpers are defined for plausible compositions
+  // including the following example for single-precision GEMM. Typical values are used as
+  // default template arguments. See `cutlass/gemm/device/default_gemm_configuration.h` for more details.
+  //
+  // To view the full gemm device API interface, see `cutlass/gemm/device/gemm.h`
+
+  using RowMajor = cutlass::layout::RowMajor;
+
+  using CutlassGemm = cutlass::gemm::device::Gemm<float,        // Data-type of A matrix
+                                                  RowMajor,  // Layout of A matrix
+                                                  float,        // Data-type of B matrix
+                                                  RowMajor,  // Layout of B matrix
+                                                  float,        // Data-type of C matrix
+                                                  RowMajor>; // Layout of C matrix
+
+  // Define a CUTLASS GEMM type
+  CutlassGemm gemm_operator;
+
+  // Construct the CUTLASS GEMM arguments object.
+  //
+  // One of CUTLASS's design patterns is to define gemm argument objects that are constructible
+  // in host code and passed to kernels by value. These may include pointers, strides, scalars,
+  // and other arguments needed by Gemm and its components.
+  //
+  // The benefits of this pattern are (1.) a structured, composable strategy for passing host-constructible
+  // arguments to kernels and (2.) minimized initialization overhead on kernel entry.
+  //
+  CutlassGemm::Arguments args({M , N, K},  // Gemm Problem dimensions
+                              {A, lda},    // Tensor-ref for source matrix A
+                              {B, ldb},    // Tensor-ref for source matrix B
+                              {C, ldc},    // Tensor-ref for source matrix C
+                              {C, ldc},    // Tensor-ref for destination matrix D (may be different memory than source C matrix)
+                              {alpha, beta}); // Scalars used in the Epilogue
+
+  //
+  // Launch the CUTLASS GEMM kernel.
+  //
+  
+  cutlass::Status status = gemm_operator(args);
+
+  //
+  // Return a cudaError_t if the CUTLASS GEMM operator returned an error code.
+  //
+
+  if (status != cutlass::Status::kSuccess) {
+    return cudaErrorUnknown;
+  }
+
+  // Return success, if no errors were encountered.
+  return cudaSuccess;
+}
+
+/// Kernel to initialize a matrix with small integers.
+__global__ void InitializeMatrix_kernel(
+  float *matrix,
+  int rows,
+  int columns,
+  int seed = 0) {
+
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (i < rows && j < columns) {
+    int offset = i + j * rows;
+
+    // Generate arbitrary elements.
+    int const k = 16807;
+    int const m = 16;
+    float value = float(((offset + seed) * k % m) - m / 2);
+
+    matrix[offset] = value;
+  }
+}
+
+/// Simple function to initialize a matrix to arbitrary small integers.
+cudaError_t InitializeMatrix(float *matrix, int rows, int columns, int seed = 0) {
+
+  dim3 block(16, 16);
+  dim3 grid(
+    (rows + block.x - 1) / block.x,
+    (columns + block.y - 1) / block.y
+  );
+
+  InitializeMatrix_kernel<<< grid, block >>>(matrix, rows, columns, seed);
+
+  return cudaGetLastError();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Allocates device memory for a matrix then fills with arbitrary small integers.
+cudaError_t AllocateMatrix(float **matrix, int rows, int columns, int seed = 0) {
+  cudaError_t result;
+
+  size_t sizeof_matrix = sizeof(float) * rows * columns;
+
+  // Allocate device memory.
+  result = cudaMalloc(reinterpret_cast<void **>(matrix), sizeof_matrix);
+
+  if (result != cudaSuccess) {
+    std::cerr << "Failed to allocate matrix: "
+      << cudaGetErrorString(result) << std::endl;
+    return result;
+  }
+
+  // Clear the allocation.
+  result = cudaMemset(*matrix, 0, sizeof_matrix);
+
+  if (result != cudaSuccess) {
+    std::cerr << "Failed to clear matrix device memory: "
+      << cudaGetErrorString(result) << std::endl;
+    return result;
+  }
+
+  // Initialize matrix elements to arbitrary small integers.
+  result = InitializeMatrix(*matrix, rows, columns, seed);
+
+  if (result != cudaSuccess) {
+    std::cerr << "Failed to initialize matrix: "
+      << cudaGetErrorString(result) << std::endl;
+    return result;
+  }
+
+  return result;
+}
---
+++ examples/30_naive_gemm/naive_gemm.cu
@@ -0,0 +1,229 @@
+#include <algorithm>
+#include <cstdio>
+#include "cutlass_gemm.h"
+#include "cutlass/util/debug.h"
+#include <exception>
+#include <vector>
+
+struct GemmParams
+{
+    int M;
+    int N;
+    int K;
+    void *ptrA;
+    void *ptrB;
+    void *ptrC;
+    void *ptrD; 
+    float alpha;
+    float beta;
+};
+
+template<int M_, int N_, int K_>
+struct CtaTile
+{
+    static constexpr int M = M_;
+    static constexpr int N = N_;
+    static constexpr int K = K_;
+};
+
+template<int M_, int N_, int K_>
+struct ThreadTile
+{
+    static constexpr int M = M_;
+    static constexpr int N = N_;
+    static constexpr int K = K_;
+};
+
+__host__ __device__ int divUp(int a, int b)
+{
+    return (a+b-1)/b;
+}
+
+#define DEBUG 0
+
+// D = alpha * (A @ B + C) + beta
+// @ means matric multiply
+template<typename CtaTileT, typename ThreadTileT>
+__global__ void gemm(GemmParams params)
+{
+    if(blockIdx.x >= divUp (params.M, CtaTileT::M) || blockIdx.y >= divUp(params.N, CtaTileT::N))
+    {
+        return;
+    }
+    float accmulator [ThreadTileT::M][ThreadTileT::N];
+
+    // TODO: load accumlator init from C, assume C = 0 for now
+    for(int m=0; m<ThreadTileT::M; ++m)
+    {
+        for(int n=0; n<ThreadTileT::N; ++n)
+        {
+            accmulator[m][n] = 0;
+        }
+    }
+
+    __shared__ float tileA [CtaTileT::M][CtaTileT::K];
+    __shared__ float tileB [CtaTileT::K][CtaTileT::N];
+
+    for(int ctaK = 0; ctaK < params.K; ctaK += CtaTileT::K)
+    {
+        //1. load A, B to shmem by all threads in this CTA
+        if(threadIdx.x == 0 && threadIdx.y == 0)
+        {
+            int offsetM = blockIdx.x * CtaTileT::M;
+            int offsetN = blockIdx.y * CtaTileT::N;
+            for(int m=0; m < CtaTileT::M; m++)
+            {
+                for(int k=0; k < CtaTileT::K; k++)
+                {
+                    // Assuming A is row major
+                    int addrA = (offsetM + m) * params.K + (ctaK + k);
+                    if(offsetM + m >= params.M || ctaK+k >= params.K)
+                    {
+                        tileA[m][k] = 0;
+                    }
+                    else
+                    {
+                        tileA[m][k] = reinterpret_cast<float*>(params.ptrA)[addrA];
+                    }
+                }
+            }
+            for(int k=0; k < CtaTileT::K; k++)
+            {
+                for(int n=0; n < CtaTileT::N; n++)
+                {
+                    // Assuming B is row major
+                    if(ctaK +k  >= params.K ||  offsetN + n >= params.N)
+                    {
+                        tileB[k][n] = 0;
+                    }
+                    else
+                    {
+                        int addrB = (ctaK + k) * params.N + offsetN + n;
+                        tileB[k][n] = reinterpret_cast<float*>(params.ptrB)[addrB];
+                    }
+                }
+            }
+        }
+
+        //2. sync cta
+        __syncthreads();
+
+        constexpr int InstructionM = 8;
+        constexpr int InstructionN = 8;
+        //3. do shmem gemm by all threds in this CTA
+
+        // 32/8
+        for(int instructM =0; instructM < ThreadTileT::M; instructM += InstructionM)
+        {
+            // 8/8
+            for(int instructN = 0; instructN < ThreadTileT::N; instructN += InstructionN)
+            {
+                for(int threadK = 0; threadK < CtaTileT::K; ++threadK)
+                {
+                    float fragementA [InstructionM];
+                    float fragementB [InstructionN];
+                    static_assert(ThreadTileT::K ==  1, "Only support ThreadTile::K ==1 for now");
+
+                    // fragment A = load A from shmem
+                    // fragment B = load B from shmem
+                    int offsetM = threadIdx.x * ThreadTileT::M + instructM;
+                    for(int m = 0; m < InstructionM; m++)
+                    {
+                        fragementA[m] = tileA[offsetM+m][threadK];
+                    }
+
+                    int offsetN = threadIdx.y * ThreadTileT::N + instructN;
+                    for(int n = 0; n < InstructionN; n++)
+                    {
+                       fragementB[n] = tileB[threadK][offsetN+n];
+                    }
+
+                    for(int m = 0; m < InstructionM; m++)
+                    {
+                        for(int n=0; n < InstructionN; n++)
+                        {
+                            accmulator[instructM+m][instructN+n] += fragementA[m]* fragementB[n];
+#if DEBUG
+                            if(blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0 && threadIdx.y == 0)
+                            {
+                                CUDA_LOG("accumlator[%d][%d] %f\n", m, n, accmulator[m][n]);
+                            }
+#endif
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    int offsetM = blockIdx.x * CtaTileT::M + threadIdx.x * ThreadTileT::M;
+    int offsetN = blockIdx.y * CtaTileT::N + threadIdx.y * ThreadTileT::N;
+
+    // D = alpha * C + beta
+    #pragma unroll ThreadTileT::M
+    for(int m=0; m<ThreadTileT::M; ++m)
+    {
+        #pragma unroll ThreadTileT::M
+        for(int n=0; n<ThreadTileT::N; ++n)
+        {
+            accmulator[m][n] = params.alpha * accmulator[m][n] + params.beta;
+            //store accumulator into global memory
+            int globalOffsetD = (offsetM+m) * params.N + offsetN +n;
+            reinterpret_cast<float*>(params.ptrD)[globalOffsetD] = accmulator[m][n];
+        }
+    }
+}
+
+
+int main()
+{
+    int M = 4096;
+    int N = 4096;
+    int K = 256;
+    float *A ;
+    float *B ;
+    float *C ;
+    float *RefC ;
+
+    std::vector<float> hC (M*N, 0);
+    std::vector<float> hRefC (M*N, -1);
+
+    CUDA_PERROR(AllocateMatrix(&A, M, K, 10));
+    CUDA_PERROR(AllocateMatrix(&B, K, N, 101));
+    CUDA_PERROR(AllocateMatrix(&C, M, N, 10000));
+    CUDA_PERROR(AllocateMatrix(&RefC, M, N, 103));
+
+    float alpha = 1;
+    float beta = 0;
+    GemmParams params {M, N, K, A, B, C, C, alpha, beta};
+
+    // All fixed.
+    using CtaTileShape = CtaTile<256, 256, 16>;
+    using ThreadTileShape = ThreadTile<32, 8, 1>;
+    dim3 block = {8, 32};
+
+    dim3 grid = {divUp(M, CtaTileShape::M), divUp(N, CtaTileShape::N), 1};
+
+    gemm<CtaTileShape, ThreadTileShape> <<<grid, block, 0>>>(params);
+    CUDA_PERROR(cudaDeviceSynchronize());
+    CUDA_PERROR(cudaMemcpy(hC.data(), C, hC.size()*sizeof(float), cudaMemcpyDeviceToHost));
+
+    CutlassSgemmTT(M, N, K, alpha, A, K, B, N, beta, RefC, N);
+    CUDA_PERROR(cudaMemcpy(hRefC.data(), RefC, hRefC.size()*sizeof(float), cudaMemcpyDeviceToHost));
+
+    bool eq = std::equal(hC.begin(), hC.end(), hRefC.begin(), 
+        [](float a, float b) { 
+#if DEBUG
+            printf("a: %f, b: %f\n", a, b);
+#endif
+            auto const err = std::abs(a-b);
+            if(err >=1e-3 && err >= 0.05*std::max(std::abs(a), std::abs(b)))
+            {
+                printf("a: %f, b: %f\n", a, b);
+                return false;
+            }
+            return true;
+        });
+    printf(eq ? "Check Pass\n" : "Check Error\n");
+    return !eq;
+}
